1b84bc410fba7b1f848b8134d87a55f43f7261b7
scwong85 <sweechong.wong@gmail.com>
2026-02-27 13:38:38 +0200

    add service worker

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'workout-tracker-v1';
+const CACHE_URLS = [
+  './',
+  './WorkoutTracker.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_URLS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
